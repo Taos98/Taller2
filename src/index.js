56bb3f5dc9cca6e3b5fa4369e6534b18a9091eaa
Taos98 <tomasmmontecinos@gmail.com>
2021-10-08 00:52:08 -0300

    Taller finalizado

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import MiComponente from './vistas/formulario/MiComponente';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MiComponente />
   </React.StrictMode>,
   document.getElementById('root')
 );
